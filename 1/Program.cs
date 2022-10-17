5cc8eb32e3fad77e9ad3ff8bb2643ad98746932f
Maria <mariayarema1eerr@gmail.com>
2022-10-17 09:32:54 +0300

    Добавьте файлы проекта.

---
+++ 1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Text;
+
+
+public class Program
+{
+    static void Main()
+    {
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+        try
+        {
+            Child child = new Child(name, age);
+            Console.WriteLine(child);
+        }
+        catch (ArgumentException ae)
+        {
+            Console.WriteLine(ae.Message);
+        }
+    }
+}
+public class Person
+{
+    private int age;
+    private string name;
+
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public virtual int Age
+    {
+        get => this.age;
+        protected set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Age must be positive!");
+            }
+
+            this.age = value;
+        }
+    }
+
+    public virtual string Name
+    {
+        get => this.name;
+        protected set
+        {
+            if (value.Length < 3 || string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Name's length should not be less than 3 symbols!");
+            }
+
+            this.name = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append($"Name: {this.Name}, Age: {this.Age}");
+
+        return stringBuilder.ToString();
+    }
+
+}
+public class Child : Person
+{
+    public Child(string name, int age)
+        : base(name, age)
+    {
+
+    }
+
+    public override int Age
+    {
+        get => base.Age;
+        protected set
+        {
+            if (value > 15)
+            {
+                throw new ArgumentException("Child's age must be less than 15!");
+            }
+
+            base.Age = value;
+        }
+    }
+
+}
---
+++ 2/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            string author = Console.ReadLine();
+            string title = Console.ReadLine();
+            decimal price = decimal.Parse(Console.ReadLine());
+
+            Book book = new Book(author, title, price);
+            GoldenEditionBook goldenEditionBook = new GoldenEditionBook(author, title, price);
+
+            Console.WriteLine(book);
+            Console.WriteLine(goldenEditionBook);
+        }
+        catch (ArgumentException ae)
+        {
+            Console.WriteLine(ae.Message);
+        }
+
+    }
+}
+
+
+public class Book
+{
+    private string title;
+    private string author;
+    private decimal price;
+
+    public Book(string author, string title, decimal price)
+    {
+        this.Title = title;
+        this.Author = author;
+        this.Price = price;
+    }
+
+    public string Author
+    {
+        get => this.author;
+        protected set
+        {
+            var indexOf = value.IndexOf(' ');
+            if (char.IsDigit(value[indexOf + 1]))
+            {
+                throw new ArgumentException("Author not valid!");
+            }
+
+            this.author = value;
+        }
+    }
+
+    public string Title
+    {
+        get => this.title;
+        protected set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Title not valid!");
+            }
+
+            this.title = value;
+        }
+    }
+
+    public virtual decimal Price
+    {
+        get => this.price;
+        protected set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Price not valid!");
+            }
+
+            this.price = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        var resultBuilder = new StringBuilder();
+        resultBuilder.AppendLine($"Type: {this.GetType().Name}")
+        .AppendLine($"Title: {this.Title}")
+        .AppendLine($"Author: {this.Author}")
+        .AppendLine($"Price: {this.Price:f2}");
+        string result = resultBuilder.ToString().TrimEnd();
+        return result;
+    }
+
+}
+public class GoldenEditionBook : Book
+{
+
+    public GoldenEditionBook(string title, string author, decimal price)
+        : base(title, author, price)
+    {
+
+    }
+
+    public override decimal Price
+    {
+        get => base.Price * 1.3m;
+    }
+}
+
